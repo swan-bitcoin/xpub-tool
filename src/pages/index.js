20c5556b90b6a4d5e471946f7b68a4a18718f7a5
Gigi <dergigi@pm.me>
2020-08-18 16:00:55 +0200

    Use range input for address count

--- src/pages/index.js
+++ src/pages/index.js
@@ -164,29 +164,45 @@ class XPubInput extends React.Component {
               onChange={this.handleXpubChange}
             />
           </Form.Group>
-          <Form.Group>
-            <Form.Control
-              as="select"
-              size="sm"
-              value={this.state.accountNumber}
-              onChange={this.handleAccountNumberChange}
-            >
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-              <option>9</option>
-              <option>10</option>
-              <option>11</option>
-              <option>12</option>
-              <option>13</option>
-              <option>14</option>
-              <option>15</option>
-            </Form.Control>
+          <Form.Group as={Row}>
+            <Form.Label column sm="2">
+              Account Nr.
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                as="select"
+                size="sm"
+                value={this.state.accountNumber}
+                onChange={this.handleAccountNumberChange}
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>
+                <option>13</option>
+                <option>14</option>
+                <option>15</option>
+              </Form.Control>
+            </Col>
+            <Form.Label column sm="2">
+              Addresses
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                type="range"
+                name="addresCount"
+                value={this.state.address}
+                onChange={this.handleAddressCountChange}
+              />
+            </Col>
           </Form.Group>
         </Form>
         {/* xPub:
