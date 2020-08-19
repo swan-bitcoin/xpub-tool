a495920909785f243a09f198a74793d70a614b4e
Gigi <dergigi@pm.me>
2020-08-19 15:18:01 +0200

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -10,7 +10,7 @@ const CustomNavbar = ({ pageInfo }) => {
       <Navbar variant="dark" expand="lg" id="site-navbar">
         {/* <Container> */}
         <Link to="/" className="link-no-style">
-          <Navbar.Brand as="span">Mainnet</Navbar.Brand>
+          <Navbar.Brand as="span">BIP 44</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
