5938d3e0214c2c6a6da097eb6336257f7a454908
Gigi <dergigi@pm.me>
2020-08-21 15:07:48 +0200

    Add account selection to HWW xpub extraction

--- src/pages/hww.js
+++ src/pages/hww.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Tabs, Tab, Row, Col, Container } from "react-bootstrap"
+import React, { useState, useMemo } from "react"
+import { Form, Tabs, Tab, Row, Col, Container } from "react-bootstrap"
 import {
   MAINNET,
   // TESTNET,
@@ -21,57 +21,67 @@ const separator = "/"
 // m / purpose' / coin_type' / account' / change / address_index
 // Example: "m/44'/0'/0'"
 const bipPrefixes = [44, 49, 84]
-const acounts = [0, 1, 2, 3]
-const bip32Paths = bipPrefixes.map(purpose => {
-  return [coinPrefix, harden(purpose), networkPrefix, harden(0)].join(separator)
-})
-
-function bip32Permutations(
-  depth = 2,
-  bipPrefixes = [44, 49, 84],
-  isHardened = true
-) {
-  let permutations = []
-  for (const purpose of bipPrefixes) {
-    let path = [
-      coinPrefix,
-      isHardened ? harden(purpose) : purpose,
-      networkPrefix,
-    ]
-    for (let i = 0; i < depth; i++) {
-      path.push(isHardened ? harden(0) : 0)
-      permutations.push(path.join(separator))
-    }
-  }
-  return permutations
-}
 
 function harden(string) {
   return string + "'"
 }
 
-const HWW = () => (
-  <Layout pageInfo={{ pageName: "hww" }}>
-    <Tabs id="hardware-wallet-selector">
-      {[LEDGER, TREZOR].map(type => (
-        <Tab eventKey={type} title={type.toUpperCase()}>
-          {bip32Paths.map(path => (
-            <HardwareWalletExtendedPublicKeyImporter
-              key={path}
-              network={network}
-              bip32Path={path}
-              keystore={type}
-            />
+const HWW = props => {
+  const [accountNumber, setAccountNumber] = useState(0)
+  let bip32Paths = bipPrefixes.map(purpose => {
+    return [
+      coinPrefix,
+      harden(purpose),
+      networkPrefix,
+      harden(accountNumber),
+    ].join(separator)
+  })
+
+  const handleAccountNumberChange = event =>
+    setAccountNumber(event.target.value)
+
+  let accountList = []
+  for (var i = 0; i < 25; i++) {
+    accountList.push(<option key={i}>{i}</option>)
+  }
+
+  return (
+    <Layout pageInfo={{ pageName: "hww" }}>
+      <Container>
+        <Form>
+          <Form.Group as={Row}>
+            <Form.Label column sm="2">
+              Account Nr.
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                as="select"
+                size="sm"
+                value={accountNumber}
+                onChange={handleAccountNumberChange}
+              >
+                {accountList}
+              </Form.Control>
+            </Col>
+          </Form.Group>
+        </Form>
+        <Tabs id="hardware-wallet-selector">
+          {[LEDGER, TREZOR].map(type => (
+            <Tab eventKey={type} title={type.toUpperCase()}>
+              {bip32Paths.map(path => (
+                <HardwareWalletExtendedPublicKeyImporter
+                  key={path}
+                  network={network}
+                  bip32Path={path}
+                  keystore={type}
+                />
+              ))}
+            </Tab>
           ))}
-        </Tab>
-      ))}
-    </Tabs>
-    <Container className="text-center">
-      <Row>
-        <Col></Col>
-      </Row>
-    </Container>
-  </Layout>
-)
+        </Tabs>
+      </Container>
+    </Layout>
+  )
+}
 
 export default HWW
