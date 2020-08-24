3629aab1c634e44bd0da10b942bc260097eae23f
Gigi <dergigi@pm.me>
2020-08-24 17:54:27 +0200

    Remove unused imports

--- src/pages/hww.js
+++ src/pages/hww.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react"
+import React, { useState } from "react"
 import { Form, Tabs, Tab, Row, Col, Container } from "react-bootstrap"
 import {
   MAINNET,
