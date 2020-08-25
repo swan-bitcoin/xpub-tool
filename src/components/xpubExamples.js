6012d8cf491d1694229aee68969ee864c9b83c91
Gigi <dergigi@pm.me>
2020-08-25 15:32:45 +0200

    Extract xpubExamples

---
+++ src/components/xpubExamples.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+import { MAINNET, networkLabel } from "unchained-bitcoin"
+
+// Mainnet: xpub...
+// Testnet: tpub...
+const EXAMPLE_XPUBS = [
+  "xpub6CCHViYn5VzKSmKD9cK9LBDPz9wBLV7owXJcNDioETNvhqhVtj3ABnVUERN9aV1RGTX9YpyPHnC4Ekzjnr7TZthsJRBiXA4QCeXNHEwxLab",
+  "xpub6D7NqpxWckGwCHhpXoL4pH38m5xVty62KY2wUh6JoyDCofwHciDRoQ3xm7WAg2ffpHaC6X4bEociYq81niyNUGhCxEs6fDFAd1LPbEmzcAm",
+  "xpub6BfKpqjTwvH21wJGWEfxLppb8sU7C6FJge2kWb9315oP4ZVqCXG29cdUtkyu7YQhHyfA5nt63nzcNZHYmqXYHDxYo8mm1Xq1dAC7YtodwUR",
+]
+const EXAMPLE_TPUBS = [
+  "tpubDCZv1xNTnmwmXe3BBMyXekiVreY853jFeC8k9AaEAqCDYi1ZTSTLH3uQonwCTRk9jL1SFu1cLNbDY76YtcDR8n2inSMwBEAdZs37EpYS9px",
+]
+
+const XPubExamples = props => {
+  const pubs = props.network === MAINNET ? EXAMPLE_XPUBS : EXAMPLE_TPUBS
+
+  return (
+    <div>
+      <p>
+        The following are some random example xpubs for easy testing (
+        {networkLabel(props.network)}):
+      </p>
+      <ul>
+        {pubs.map((pub, i) => (
+          <li key={i}>
+            <code>{pub}</code>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export { XPubExamples, EXAMPLE_XPUBS, EXAMPLE_TPUBS }
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,30 +10,19 @@ import {
   Table,
   Form,
 } from "react-bootstrap"
-import {
-  MAINNET,
-  TESTNET,
-  networkLabel,
-  validateExtendedPublicKey,
-} from "unchained-bitcoin"
+import { MAINNET, TESTNET, validateExtendedPublicKey } from "unchained-bitcoin"
 
 import Layout from "../components/layout"
+import {
+  XPubExamples,
+  EXAMPLE_XPUBS,
+  EXAMPLE_TPUBS,
+} from "../components/xpubExamples"
 import { DerivedAddress, Purpose } from "../lib/xpub.js"
 import { accountDerivationPath } from "../lib/paths.js"
 
 const DEFAULT_NETWORK = MAINNET // or TESTNET
 const MAX_ACCOUNTS = 25
-
-// Mainnet: xpub...
-// Testnet: tpub...
-const EXAMPLE_XPUBS = [
-  "xpub6CCHViYn5VzKSmKD9cK9LBDPz9wBLV7owXJcNDioETNvhqhVtj3ABnVUERN9aV1RGTX9YpyPHnC4Ekzjnr7TZthsJRBiXA4QCeXNHEwxLab",
-  "xpub6D7NqpxWckGwCHhpXoL4pH38m5xVty62KY2wUh6JoyDCofwHciDRoQ3xm7WAg2ffpHaC6X4bEociYq81niyNUGhCxEs6fDFAd1LPbEmzcAm",
-  "xpub6BfKpqjTwvH21wJGWEfxLppb8sU7C6FJge2kWb9315oP4ZVqCXG29cdUtkyu7YQhHyfA5nt63nzcNZHYmqXYHDxYo8mm1Xq1dAC7YtodwUR",
-]
-const EXAMPLE_TPUBS = [
-  "tpubDCZv1xNTnmwmXe3BBMyXekiVreY853jFeC8k9AaEAqCDYi1ZTSTLH3uQonwCTRk9jL1SFu1cLNbDY76YtcDR8n2inSMwBEAdZs37EpYS9px",
-]
 
 const IndexPage = props => {
   const networks = [MAINNET, TESTNET]
@@ -100,26 +89,6 @@ const IndexPage = props => {
         </Row>
       </Container>
     </Layout>
-  )
-}
-
-const XPubExamples = props => {
-  const pubs = props.network === MAINNET ? EXAMPLE_XPUBS : EXAMPLE_TPUBS
-
-  return (
-    <div>
-      <p>
-        The following are some random example xpubs for easy testing (
-        {networkLabel(props.network)}):
-      </p>
-      <ul>
-        {pubs.map((pub, i) => (
-          <li key={i}>
-            <code>{pub}</code>
-          </li>
-        ))}
-      </ul>
-    </div>
   )
 }
 
