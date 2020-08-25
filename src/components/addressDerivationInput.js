14ef66ba08d9da4b7fbae073afb27820910c53f6
Gigi <dergigi@pm.me>
2020-08-25 16:13:36 +0200

    Extract address derivation input

---
+++ src/components/addressDerivationInput.js
@@ -0,0 +1,74 @@
+import React from "react"
+import { Row, Col, Form } from "react-bootstrap"
+import { Purpose } from "../lib/xpub.js"
+
+const AddressDerivationInput = props => {
+  const MAX_ACCOUNTS = 25
+  let accountList = []
+  for (var i = 0; i < MAX_ACCOUNTS; i++) {
+    accountList.push(<option key={i}>{i}</option>)
+  }
+
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Control
+          size="lg"
+          type="text"
+          placeholder="xpub..."
+          value={props.xpub}
+          onChange={props.xpubHandler}
+        />
+      </Form.Group>
+      <Form.Group as={Row}>
+        <Form.Label column sm="2">
+          BIP
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control
+            as="select"
+            size="sm"
+            name="purpose"
+            value={props.purpose}
+            onChange={props.purposeHandler}
+          >
+            {Object.values(Purpose).map(type => (
+              <option key={type}>{type}</option>
+            ))}
+          </Form.Control>
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row}>
+        <Form.Label column sm="2">
+          Account Nr.
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control
+            as="select"
+            size="sm"
+            name="accountNumber"
+            value={props.accountNumber}
+            onChange={props.accountNumberHandler}
+          >
+            {accountList}
+          </Form.Control>
+        </Col>
+        <Form.Label column sm="2">
+          Stacking Time
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control
+            type="range"
+            name="addressCount"
+            min="1"
+            max="99"
+            value={props.addressCount}
+            onChange={props.addressCountHandler}
+          />
+        </Col>
+      </Form.Group>
+    </Form>
+  )
+}
+
+export default AddressDerivationInput
--- src/components/xpubTool.js
+++ src/components/xpubTool.js
@@ -1,12 +1,11 @@
 import React, { useState, useMemo } from "react"
-import { Alert, Row, Col, Form } from "react-bootstrap"
+import { Alert } from "react-bootstrap"
 import { MAINNET, validateExtendedPublicKey } from "unchained-bitcoin"
 import { EXAMPLE_XPUBS, EXAMPLE_TPUBS } from "../components/xpubExamples"
 import DerivedAddressesTable from "../components/derivedAddressesTable.js"
+import AddressDerivationInput from "../components/addressDerivationInput.js"
 import { accountDerivationPath } from "../lib/paths.js"
 import { Purpose } from "../lib/xpub.js"
-
-const MAX_ACCOUNTS = 25
 
 const XPubTool = props => {
   const exampleXPub =
@@ -29,71 +28,18 @@ const XPubTool = props => {
     setAccountNumber(event.target.value)
   const handleAddressCountChange = event => setAddressCount(event.target.value)
 
-  let accountList = []
-  for (var i = 0; i < MAX_ACCOUNTS; i++) {
-    accountList.push(<option key={i}>{i}</option>)
-  }
-
   return (
     <div>
-      <Form>
-        <Form.Group>
-          <Form.Control
-            size="lg"
-            type="text"
-            placeholder="xpub..."
-            value={xpub}
-            onChange={handleXpubChange}
-          />
-        </Form.Group>
-        <Form.Group as={Row}>
-          <Form.Label column sm="2">
-            BIP
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control
-              as="select"
-              size="sm"
-              name="purpose"
-              value={purpose}
-              onChange={handlePurposeChange}
-            >
-              {Object.values(Purpose).map(type => (
-                <option key={type}>{type}</option>
-              ))}
-            </Form.Control>
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row}>
-          <Form.Label column sm="2">
-            Account Nr.
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control
-              as="select"
-              size="sm"
-              name="accountNumber"
-              value={accountNumber}
-              onChange={handleAccountNumberChange}
-            >
-              {accountList}
-            </Form.Control>
-          </Col>
-          <Form.Label column sm="2">
-            Stacking Time
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control
-              type="range"
-              name="addressCount"
-              min="1"
-              max="99"
-              value={addressCount}
-              onChange={handleAddressCountChange}
-            />
-          </Col>
-        </Form.Group>
-      </Form>
+      <AddressDerivationInput
+        xpub={xpub}
+        purpose={purpose}
+        accountNumber={accountNumber}
+        addressCount={addressCount}
+        xpubHandler={handleXpubChange}
+        purposeHandler={handlePurposeChange}
+        accountNumberHandler={handleAccountNumberChange}
+        addressCountHandler={handleAddressCountChange}
+      />
       <p>
         <code>{accountDerivationPath(purpose, accountNumber)}/i</code>
       </p>
