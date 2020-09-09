615b7aefa924cd71ce176c406f2b88c0b06073b0
Gigi <dergigi@pm.me>
2020-09-09 15:55:50 +0200

    test(lib): test xpub for mainnet

--- packages/xpub-lib/src/xpub.test.js
+++ packages/xpub-lib/src/xpub.test.js
@@ -1,10 +1,12 @@
+import { NETWORKS } from "unchained-bitcoin"
 import { isValidXpub } from "./xpub"
 
 describe("xpub", () => {
   test("xpub is valid", () => {
     expect(
       isValidXpub(
-        "xpub6CCHViYn5VzKSmKD9cK9LBDPz9wBLV7owXJcNDioETNvhqhVtj3ABnVUERN9aV1RGTX9YpyPHnC4Ekzjnr7TZthsJRBiXA4QCeXNHEwxLab"
+        "xpub6CCHViYn5VzKSmKD9cK9LBDPz9wBLV7owXJcNDioETNvhqhVtj3ABnVUERN9aV1RGTX9YpyPHnC4Ekzjnr7TZthsJRBiXA4QCeXNHEwxLab",
+        NETWORKS.MAINNET
       )
     ).toBeTruthy()
   })
