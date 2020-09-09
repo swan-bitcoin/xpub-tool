b2e0e6b292e3f68ab6391c5f39a4ac6d06241c4c
Gigi <dergigi@pm.me>
2020-09-09 18:12:58 +0200

    test(lib): add tests for legacy and native segwit paths

--- packages/xpub-lib/src/paths.test.js
+++ packages/xpub-lib/src/paths.test.js
@@ -1,6 +1,17 @@
 import { humanReadableDerivationPath } from "./paths"
 
 describe("humanReadableDerivationPath", () => {
+  test("Legacy", () => {
+    expect(humanReadableDerivationPath({ bip32Path: "m/44'/0'/0'/0/1" })).toBe(
+      "Account #1 (Legacy)"
+    )
+    expect(humanReadableDerivationPath({ bip32Path: "m/44'/0'/1'/0/1" })).toBe(
+      "Account #2 (Legacy)"
+    )
+    expect(humanReadableDerivationPath({ bip32Path: "m/44'/0'/2'/0/1" })).toBe(
+      "Account #3 (Legacy)"
+    )
+  })
   test("SegWit", () => {
     expect(humanReadableDerivationPath({ bip32Path: "m/49'/0'/0'/0/1" })).toBe(
       "Account #1 (SegWit)"
@@ -10,6 +21,17 @@ describe("humanReadableDerivationPath", () => {
     )
     expect(humanReadableDerivationPath({ bip32Path: "m/49'/0'/2'/0/1" })).toBe(
       "Account #3 (SegWit)"
+    )
+  })
+  test("Native SegWit", () => {
+    expect(humanReadableDerivationPath({ bip32Path: "m/84'/0'/0'/0/1" })).toBe(
+      "Account #1 (Native SegWit)"
+    )
+    expect(humanReadableDerivationPath({ bip32Path: "m/84'/0'/1'/0/1" })).toBe(
+      "Account #2 (Native SegWit)"
+    )
+    expect(humanReadableDerivationPath({ bip32Path: "m/84'/0'/2'/0/1" })).toBe(
+      "Account #3 (Native SegWit)"
     )
   })
 })
