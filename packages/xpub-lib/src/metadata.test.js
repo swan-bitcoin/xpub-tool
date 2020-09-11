8b87a9c80408c1c85a007edfb803bc612628f633
Gigi <dergigi@pm.me>
2020-09-11 18:10:40 +0200

    test(lib): add tests for getNetworkFromXpub

--- packages/xpub-lib/src/metadata.test.js
+++ packages/xpub-lib/src/metadata.test.js
@@ -1,5 +1,22 @@
+import { NETWORKS } from "unchained-bitcoin"
 import { KEYS } from "../test/fixtures"
-import { getXpubType } from "./metadata"
+import { getXpubType, getNetworkFromXpub, getXpubMetadata } from "./metadata"
+
+describe("getNetworkFromXpub", () => {
+  test("xpub/ypub/zpub should be mainnet", () => {
+    expect(getNetworkFromXpub(KEYS.MAIN.XPUB[0])).toBe(NETWORKS.MAINNET)
+    expect(getNetworkFromXpub(KEYS.MAIN.YPUB[0])).toBe(NETWORKS.MAINNET)
+    expect(getNetworkFromXpub(KEYS.MAIN.ZPUB[0])).toBe(NETWORKS.MAINNET)
+  })
+  test("tpub/upub/vpub should be mainnet", () => {
+    expect(getNetworkFromXpub(KEYS.TEST.TPUB[0])).toBe(NETWORKS.TESTNET)
+    expect(getNetworkFromXpub(KEYS.TEST.UPUB[0])).toBe(NETWORKS.TESTNET)
+    expect(getNetworkFromXpub(KEYS.TEST.VPUB[0])).toBe(NETWORKS.TESTNET)
+  })
+  test("invalid key should be undefined", () => {
+    expect(getNetworkFromXpub("abc")).toBeFalsy()
+  })
+})
 
 describe("getXpubType", () => {
   test("xpub/tpub should be of type P2PKH (BIP 44)", () => {
