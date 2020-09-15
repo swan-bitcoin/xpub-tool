385aaa1f6b5067750f9dce7e6ec9af5085832a2d
Gigi <dergigi@pm.me>
2020-09-15 17:52:58 +0200

    docs(lib): add conversion docs

--- packages/xpub-lib/src/conversion.js
+++ packages/xpub-lib/src/conversion.js
@@ -1,6 +1,17 @@
 import { NETWORKS, convertExtendedPublicKey } from "unchained-bitcoin"
 import { TYPE } from "./types"
 
+/**
+ * Convert any extended public key (ypub, zpub, etc.) to the XPUB format
+ * defined in BIP44. Resulting key will be an xpub (mainnet) or a tpub
+ * (testnet).
+ *
+ * @param  {string} extPubKey - the extended public key to convert
+ * @param  {string} network - the network to convert to (MAINNET or TESTNET)
+ *
+ * @returns {(string|object)} converted extended public key or error object
+ * with the failed key and error message
+ */
 function convertToBIP32(extPubKey, network) {
   const targetPrefix = network === NETWORKS.MAINNET ? TYPE.XPUB : TYPE.TPUB
   return convertExtendedPublicKey(extPubKey, targetPrefix)
