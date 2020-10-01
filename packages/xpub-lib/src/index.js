06a90a3e0a0f9c0390c7004e8edc0aa048951d67
Gigi <dergigi@pm.me>
2020-10-01 10:51:49 +0200

    refactor(lib): move index into src/

---
+++ packages/xpub-lib/src/index.js
@@ -0,0 +1,18 @@
+export { NETWORKS, networkLabel } from "unchained-bitcoin"
+export {
+  ExportExtendedPublicKey,
+  PENDING,
+  ACTIVE,
+  UNSUPPORTED,
+  LEDGER,
+  TREZOR,
+} from "unchained-wallets"
+
+export * from "./conversion"
+export * from "./derivation"
+export * from "./metadata"
+export * from "./paths"
+export * from "./purpose"
+export * from "./types"
+export * from "./utils"
+export * from "./validation"
