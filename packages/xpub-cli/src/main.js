23aaa9a1ddf6bb14296d8ace604331a1aef8323d
Gigi <dergigi@pm.me>
2020-10-05 17:14:01 +0200

    feat(cli): add derivation, purpose, verbose

--- packages/xpub-cli/src/main.js
+++ packages/xpub-cli/src/main.js
@@ -4,6 +4,8 @@ import {
   NETWORKS,
   isValidAddress,
   isValidExtPubKey,
+  addressFromExtPubKey,
+  Purpose,
 } from "@swan-bitcoin/xpub-lib"
 
 const { program } = require("commander")
@@ -12,7 +14,16 @@ program.version("0.0.1")
 program
   .option("-a, --check-address <address>", "check bitcoin address for validity")
   .option("-x, --check-xpub <xpub>", "check extended public key for validity")
+  .option(
+    "-d, --derive <xpub>",
+    "derive address(es) from an extended public key"
+  )
+  .option(
+    "-p, --purpose <purpose>",
+    "derivation purpose which dictates the address type ['p2pkh', 'p2sh', 'p2wpkh']"
+  )
   .option("-t, --testnet", "use TESTNET")
+  .option("-v, --verbose", "verbose output")
 
 program.parse(process.argv)
 
@@ -30,4 +41,32 @@ if (program.checkXpub) {
   const xpub = program.checkXpub
   const isValid = isValidExtPubKey(xpub, network)
   console.log(isValid)
+}
+if (program.derive) {
+  const extPubKey = program.derive
+  console.log(program.purpose)
+  const purpose = program.purpose
+    ? parsePurpose(program.purpose)
+    : Purpose.P2WPKH // default to P2WPKH
+  const address = addressFromExtPubKey({ extPubKey, network, purpose })
+
+  if (program.verbose) {
+    console.log(address)
+  } else {
+    console.log(address.address)
+  }
+}
+
+function parsePurpose(purpose) {
+  switch (purpose.toLowerCase()) {
+    case "p2pkh": {
+      return Purpose.P2PKH
+    }
+    case "p2sh": {
+      return Purpose.P2SH
+    }
+    case "p2wpkh": {
+      return Purpose.P2WPKH
+    }
+  }
 }
